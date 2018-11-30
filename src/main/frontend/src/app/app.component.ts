eb1202fc39494b3256c2739836a952dbaf524d61
Gladys <gladys_hilerio@msn.com>
2018-11-29 20:42:25 -0800

    first commit

---
+++ src/main/frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {EcommerceService} from "./ecommerce/services/EcommerceService";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [EcommerceService]
+})
+export class AppComponent {
+  title = 'frontend';
+}
+
